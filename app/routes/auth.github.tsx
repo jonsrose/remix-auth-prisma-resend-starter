ac0a413efc6adb76e480038a7d59c161a1d0f51e
Heisenberg <jon@jonrose.com>
2024-10-18 13:14:51 -0700

    github auth fixed

--- app/routes/auth.github.tsx
+++ app/routes/auth.github.tsx
@@ -1,10 +1,18 @@
 import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { authenticator } from "~/services/auth.server";
-import { redirect } from "@remix-run/node";
 
-export const loader: LoaderFunction = () => redirect("/login");
+export const loader: LoaderFunction = ({ request }) => {
+  console.log('GitHub Auth Loader Function');
+  return authenticator.authenticate("github", request, {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+  });
+};
 
 export const action: ActionFunction = ({ request }) => {
-  console.log('ActionFunction');
-  return authenticator.authenticate("github", request);
+  console.log('GitHub Auth Action Function');
+  return authenticator.authenticate("github", request, {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+  });
 };
