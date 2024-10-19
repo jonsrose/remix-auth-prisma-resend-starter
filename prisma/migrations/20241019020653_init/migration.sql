4bc0be6f76c7a389d1e1c84ae129f26983293da2
Heisenberg <jon@jonrose.com>
2024-10-18 19:07:21 -0700

    regnerated schema

--- prisma/migrations/20241019020653_init/migration.sql
+++ prisma/migrations/20241019020653_init/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT,
-    "password" TEXT NOT NULL,
+    "password" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
